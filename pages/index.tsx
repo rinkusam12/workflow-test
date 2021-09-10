7b10a23401f2fe194dee204e98174e9413906336
Rinku Samanta <rinkusamanta12@outlook.com>
2021-09-10 20:02:28 +0530

    changa

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   const showAlert = () => {
     console.log("whythis is not covered in sonarqube");
+    // console.log("whythis is not covered in sonarqube");
     
     const v = "dsafssa";
     alert(v);
