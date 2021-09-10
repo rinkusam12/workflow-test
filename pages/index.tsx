996ff7a127a72a103acc5b6ba4e797e472685864
Rinku Samanta <rinkusamanta12@outlook.com>
2021-09-10 16:57:29 +0530

    dsafasdf

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const showAlert = () => {
-    const v = "dsafsa";
+    const v = "dsafssa";
     alert(v);
   };
   return (
